fdc1177d560ec7e0a1b7bfc9c744ecaa3c85c1bb
Cesar G <undertaker_400@hotmail.com>
2018-11-21 13:08:48 -0600

    Primer commit

---
+++ helpers/slug.js
@@ -0,0 +1,6 @@
+import slugify from 'slugify';
+
+export default function slug(name) {
+    return slugify(name,{lower:true}).replace(/[^\w\-]+/g, '')
+    
+}
---
+++ pages/about.js
@@ -0,0 +1,46 @@
+
+export default class extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <h1>Next Js</h1>
+                    <img src="/static/React.png" alt="React" />
+                    <p>Curso de Next.js platzi </p>
+                </div>
+                <style jsx>
+                    {`
+                .container {
+                  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+                  display: flex;
+                  flex-direction: column;
+                  height: 100%;
+                  align-items: center;
+                  justify-content: space-around;
+                }
+                h1 {
+                  color: #14BDEB;
+                  font-size: 5.5em;
+                }
+                :global(div p) {
+                  color: #EEF4ED;
+                  font-size: 3.5em;
+                  text-align: center;
+                }
+                img {
+                  max-width: 70%;
+                }
+              `}
+                </style>
+
+                <style jsx global>
+                    {`
+                body {
+                  background: #2C302E;
+                }
+              `}
+                </style>
+            </div>
+        );
+    }
+}
